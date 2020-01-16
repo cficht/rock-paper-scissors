bc00ff2a9bb5c05f97cfe3fc068c92f6d56e065e
cficht <chris.ficht@gmail.com>
2020-01-15 17:54:29 -0800

    made app.js function as intended

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
 import { getRandomThrow, checkResult } from './get-random-throw.js';
 
 const playButton = document.getElementById('play-button');
-let userThrowButton = document.querySelector('input:checked');
+const winText = document.getElementById('win-text');
+const loseText = document.getElementById('lose-text');
+const drawText = document.getElementById('draw-text');
+const currentGameText = document.getElementById('current-game-text');
+const computerChoiceText = document.getElementById('computer-choice-text');
 
-let computerThrow;
+let winCount = 0;
+let loseCount = 0;
+let drawCount = 0;
 
 playButton.addEventListener('click', () => {
-    computerThrow = getRandomThrow();
-    userThrowButton = document.querySelector('input:checked');
-    let userThrow = userThrowButton.value;
+
+    let computerThrow = getRandomThrow();
+    let userThrow = document.querySelector('input:checked').value;
     
-    console.log('COMPUTER ' + computerThrow);
-    console.log('USER ' + userThrow);
+    // console.log('COMPUTER ' + computerThrow);
+    // console.log('USER ' + userThrow);
 
     let currentResult = checkResult(userThrow, computerThrow);
-    console.log(currentResult);
+
+    // console.log(currentResult);
+
+    if (currentResult === 'win') {
+        winCount++;
+        currentGameText.textContent = 'YOU WIN';
+    } else if (currentResult === 'lose') {
+        loseCount++;
+        currentGameText.textContent = 'YOU LOSE';
+    } else if (currentResult === 'draw') {
+        drawCount++;
+        currentGameText.textContent = 'YOU DRAW';
+    }
+
+    computerChoiceText.textContent = `The computer chose ${computerThrow}`;
+
+    winText.textContent = winCount;
+    loseText.textContent = loseCount;
+    drawText.textContent = drawCount;
 
 });
 
